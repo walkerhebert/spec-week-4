acac3b72d37646f5e100071c49e1196b641dbec4
Walker Hebert <walkerhebert@yahoo.com>
2022-11-07 16:32:08 -0600

    another commit

--- forms.py
+++ forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, SubmitField, TextAreaField, BooleanField, Selec
 from wtforms.validators import DataRequired, Length
 
 class TeamForm(FlaskForm):
-    team_name = StringField('team name', validators=[DataRequired(), Length(min=4, max=255)])
-    submit = SubmitField("submit")
+    team_name = StringField('Team Name', validators=[DataRequired(), Length(min=4, max=255)])
+    submit = SubmitField("Submit Team")
     
 class ProjectForm(FlaskForm):
-    project_name = StringField('project name', validators=[DataRequired(), Length(min=4, max=255)])
-    description = TextAreaField('description')
-    completed = BooleanField('completed?')
-    team_selection = SelectField('team')
-    sumbit = SubmitField('submit')
+    project_name = StringField('Project Name', validators=[DataRequired(), Length(min=4, max=255)])
+    description = TextAreaField('Description')
+    completed = BooleanField('Is it completed?')
+    team_selection = SelectField('Team Name')
+    submit = SubmitField('Submit')
     
     def update_teams(self, teams):
         self.team_selection.choices = [ (team.id, team.team_name) for team in teams]
